50b1be32bf65dbf27d6b978a2722d4cdf856d73b
arthuradriaens-code <arthur99.adriaens@gmail.com>
2025-04-09 15:52:33 +0200

    no more Data folder in root

--- tests/Scripts/compare.py
+++ tests/Scripts/compare.py
@@ -44,7 +44,7 @@ def compare_with_tolerance(file1, file2, default_tolerance=1.0, special_toleranc
 
 
 
-solution_file = "Data/Solution/output.csv"
+solution_file = "tests/Solution/output.csv"
 test_file = "Data/Test/output.csv"
 tolerance_percentage = 1.0  # 1% tolerance
 
