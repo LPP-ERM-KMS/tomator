d897f824e86f6a53e05228a26a964dec9bda2eb3
arthuradriaens-code <arthur99.adriaens@gmail.com>
2026-04-29 19:21:31 +0200

    Revert "Eigen initialization is no longer needed"
    This reverts commit db29c4757e932e1a713b46014d604563d5d9a13f.
    
    This had to be published to the devel branch

--- src/Tomator1D.cpp
+++ src/Tomator1D.cpp
@@ -49,6 +49,7 @@ int main(int argc, char *argv[]) {
 
     double tstartloop;
     double tstartcalculation = omp_get_wtime();
+    Eigen::initParallel();
     time_t now = time(0);
     char *timestamp = ctime(&now);
 
